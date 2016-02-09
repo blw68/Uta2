c8eaf3cbe2b357e136c0d07a78dfbed4cfb473dc
snakebyte72 <laszlo.glant@gmail.com>
2016-02-08 22:01:10 -0500

    Update SongViewController.java

--- SongViewController.java
+++ SongViewController.java
@@ -1,6 +1,7 @@
 package SongLibView;
 
 
+import java.io.File;
 import java.io.IOException;
 
 
@@ -19,22 +20,13 @@ import javafx.stage.Stage;
 public class SongViewController {
 
 	private SongLib songlib;
-	@FXML
-	public Button cancelButton, okButton;
+	
 
 	@FXML 
 	public ListView<Song> listView;
 
-	public ObservableList<Song> obsList;
+	ObservableList<Song> obsList = FXCollections.observableArrayList(Song.songList);
 	
-	@FXML
-	private TextField title;
-	@FXML
-	private TextField artist;
-	@FXML
-	private TextField album;
-	@FXML
-	private TextField year;
 
 	@FXML
 	private void homeScene() throws IOException {
@@ -55,74 +47,17 @@ public class SongViewController {
 	private void deleteButton() throws IOException {
 		SongLib.showDeleteScene();
 	}
-	@FXML
-	private void cancelButtonEvent(ActionEvent event) {
-		Stage stage = (Stage) cancelButton.getScene().getWindow();
-		stage.close();
+	/*ListView<Song> listView = new ListView<>();
+	ObservableList<Song> obsList = FXCollections.observableArrayList(songList);
+	System.out.println("printing obs list");
+	Song.printObsList(obsList);
+	listView.setItems(obsList);*/
+	public void initialize(){
+		File f = new File("output.txt");
+		Song.input(Song.songList, f);
+		listView.setItems(obsList);
+		Song.printList(Song.songList);
+		Song.printObsList(obsList);
 	}
-
-	@FXML
-	private void okButtonEvent(ActionEvent event) {
-		try {
-			System.out.println("ok button pressed");
-			System.out.println("title is " + title.getText());
-			System.out.println("artist is " + artist.getText());
-			System.out.println("album is " + album.getText());
-			System.out.println("year is " + year.getText());
-			
-			Song s = new Song(title.getText(), artist.getText(), album.getText(), Integer.parseInt(year.getText()));
-					
-			SongLib.addInAbcOrder(Song.songList, s);
-			
-			System.out.println("printing songList");
-			SongLib.printList(Song.songList);
-			System.out.println("after printing songList");
-			
-						
-			//obsList = FXCollections.observableArrayList();
-			ObservableList<Song> obsList = FXCollections.observableArrayList(Song.songList);
-			
-//			for (Song i : Song.songList) {
-//				obsList.add(i);
-//			}
-			
-			//listView.setItems(obsList);
-			
-			System.out.println("done");
-			
-//			obsList.add(s);
-//			
-//			listView = new ListView(obsList);
-//			
-//			listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
-//				public void changed(ObservableValue ov, Object t, Object t1) {
-//					title.setText((String) t1);
-//				}
-//			});
-			
-//			listView.getSelectionModel().select(0);
-//			
-			
-//			System.out.println("before set items in list view");
-//			listView.setItems(obsList);
-//			System.out.println("after set items in list view");
-		} catch (Exception e) {
-			System.out.println("exception in ok button event");
-			e.printStackTrace(System.out);
-		}
-
-		// close pop up window just like cancel does
-		Stage stage = (Stage) okButton.getScene().getWindow();
-		stage.close();
-	}
-
-//	@FXML
-//	private void TitleAction(ActionEvent event) {
-//		//		TextField source = (TextField)event.getSource();
-//		//		System.out.println("You entered " + source.getText());
-//
-//
-//	}
-
 	
 }
